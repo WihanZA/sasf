4476cb441e52f934f16f7ae529b8433a4b892efb
Wihan Marais <wihanmarais@sun.ac.za>
2025-05-18 09:50:51 +0200

    Add initial sasf package structure and documentation
    - Created sasf-package.R with package description and keywords
    - Defined namespace imports for sf functions

---
+++ R/sasf-package.R
@@ -0,0 +1,10 @@
+#' @description
+#' The `sasf` package makes spatial data for South Africa accessible to R users.
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom sf st_bbox
+#' @importFrom sf st_is_longlat
+## usethis namespace: end
+NULL
